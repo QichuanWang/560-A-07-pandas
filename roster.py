2d08dae0cc9a28470779b3150de1711d441e330f
QichuanWang <wqc13542@163.com>
2025-11-10 16:45:05 -0500

    Full list of 10

--- roster.py
+++ roster.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [83, 72, 73],
-          "weight": [240, 180, 180]}
+player = {"Last Name": ["Bacot", "Davis", "Cadeau", "Evans", "High", "Brown", "Dixon", "Young", "Denis", "Davis"],
+          "First Name": ["Armando", "RJ", "Elliot", "Kyan", "Zayden", "James", "Derek", "Jaydon", "Isaiah", "Elijah"],
+          "height": [83, 72, 73, 74, 82, 82, 77, 76, 76, 75],
+          "weight": [240, 180, 180, 175, 230, 240, 200, 200, 180, 205]}
 data = pd.DataFrame(player)
+
+# BMI = weight in kg / height in meters squared
+data["BMI"] = round((data ["weight"] / 2.205) / ((data ["height"] / 39.37) ** 2), 2)
+
 print(data)
+data.to_csv("bmi.csv")
